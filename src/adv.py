b052bc1d3d51ee13f31a521739b9bdd1510db963
Jessica <duellje@oregonstate.edu>
2020-09-02 18:50:33 -0400

    completed day1 mvp

--- src/adv.py
+++ src/adv.py
@@ -6,20 +6,10 @@ from room import Room
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -41,7 +31,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 player1 = Player('Jess', room['outside'])
-
+print("Welcome %s" % (player1.name))
 # Write a loop that:
 #
 # * Prints the current room name
@@ -53,13 +43,48 @@ player1 = Player('Jess', room['outside'])
 #
 # If the user enters "q", quit the game.
 
-# userDir = 'c'
+# checks that the direction provided is either n, s, e or w
 
-# while userDir != 'q':
-#     userDir = input(
-#         'Please enter a direction to move: n, s, e, w. Enter q to quit: ')
-#     if userDir.isalpha() and len(userDir) == 1:
-#         userDir = userDir.lower()
-#         print("Name and description of player's current room goes here")
-#     else:
-#         print("Please enter a single character.")
+
+def getDirection(direction):
+    switchDirections = ['n', 's', 'e', 'w']
+    for item in switchDirections:
+        if direction == item:
+            return True
+    return False
+
+
+userDir = 'c'
+
+while userDir != 'q':
+    print('\nYou find yourself in %s. %s\n' % (
+        player1.current_room.name, player1.current_room.description))
+    userDir = input(
+        'Please enter a direction to move: n, s, e, w. Enter q to quit: \n')
+    if userDir.isalpha() and len(userDir) == 1:
+        userDir = userDir.lower()
+        if getDirection(userDir):
+            if userDir == 'n':
+                try:
+                    player1.current_room = player1.current_room.n_to
+                except:
+                    print("You can't go that way\n")
+            elif userDir == 's':
+                try:
+                    player1.current_room = player1.current_room.s_to
+                except:
+                    print("You can't go that way\n")
+            elif userDir == 'e':
+                try:
+                    player1.current_room = player1.current_room.e_to
+                except:
+                    print("You can't go that way\n")
+            else:
+                try:
+                    player1.current_room = player1.current_room.w_to
+                except:
+                    print("You can't go that way\n")
+        else:
+            print('Please enter n, s, e or w\n')
+    else:
+        print("Please enter a single character.\n")
