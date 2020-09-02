0f0f160e54d538df82c60daffba665150671c631
Jessica <duellje@oregonstate.edu>
2020-09-02 15:22:34 -0400

    added room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        res = "Room(name = %s, description = %s), " % (
+            self.name, self.description)
+        return res
